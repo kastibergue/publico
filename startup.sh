9c297547c9940b1de036e49a4813cb9db1386e06
kastibergue <k45t1b@gmail.com>
2024-07-10 13:30:05 -0300

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/expect
-cd /etc/openvpn/ && ospawn openvpn us-free-178050.protonvpn.tcp.ovpn
+openvpn /etc/openvpn/us-free-178050.protonvpn.tcp.ovpn
 expect "*?assword:*"
 send "cFZf5tFngolIoRDai60jOOjeEpf1G8XO\r"
 expect eof
